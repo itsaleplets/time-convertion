401116144df0955860c43be101e3c58a9e47e82e
Alessandra <itsalematos@hotmail.com>
2024-03-17 20:28:53 -0300

    Feat: Adding time convertion logic and additional tests

--- index.test.js
+++ index.test.js
@@ -5,14 +5,29 @@ describe('Time to words', () => {
     const timeInWords = convertTimeToWords('0:00');
     expect(timeInWords).toBe('midnight');
   });
-
+ 
+ 
   it('Handles 30 - 8:30', () => {
     const timeInWords = convertTimeToWords('8:30');
     expect(timeInWords).toBe('half past eight');
   });
-
+ 
+ 
   it('Handles times after 30 mins - 2:45', () => {
     const timeInWords = convertTimeToWords('2:45');
     expect(timeInWords).toBe('quarter to three');
   });
-});
+ 
+ 
+  it('Handles times after 30 mins - 10:34', () => {
+    const timeInWords = convertTimeToWords('10:34');
+    expect(timeInWords).toBe('twenty-six to eleven');
+  });
+ 
+ 
+  it('Handles times before 30 mins - 9:03', () => {
+    const timeInWords = convertTimeToWords('9:03');
+    expect(timeInWords).toBe('three past nine');
+  });
+ });
+ 
